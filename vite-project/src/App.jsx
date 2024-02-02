48bd0185d4fc9f5a1746d2ffeac438fa1909aace
knilesh9 <neutron_stays@protonmail.com>
2024-02-02 22:01:02 +0530

    My way to make 3 button and update the id accordingly

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,9 +4,17 @@ import axios from "axios"
 
 export default function App(){
 
+  let [count, setCount] = useState(1)
+
   return<>
     <h2>Hi</h2>
-    <Todo id={2}/>
+    <button onClick={() => setCount(1)}>Id 1</button>
+    <button onClick={() => setCount(2)}>Id 2</button>
+    <button onClick={() => setCount(3)}>Id 3</button>
+    <button onClick={() => setCount(4)}>Id 4</button>
+
+    <Todo id={count}/>
+
   </>
 }
 
@@ -15,11 +23,11 @@ function Todo({id}){
   let [todo, setTodo] = useState({})
 
   useEffect( () => {
-    axios.get("https://sum-server.100xdevs.com/todo?id="+ id)
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
     .then((res) => {
       setTodo(res.data.todo)
     }) 
-  }, [])
+  }, [id])
 
   return <>
     <h1>{todo.title}</h1>
